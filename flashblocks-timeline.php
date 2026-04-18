c770bd9b1ce7ae93fc3962fe10a816ac29915608
sun <sunny@sunnymorgan.com>
2026-04-18 17:31:58 -0400

    Refactor: Normalize Flashblocks Category

--- flashblocks-timeline.php
+++ flashblocks-timeline.php
@@ -10,7 +10,7 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       flashblocks-timeline
  * Flashblocks Module: yes
- * Flashblocks Category: content
+ * Flashblocks Category: Blocks
  * Flashblocks Tags: timeline, stories
  *
  * @package FlashblocksTimeline
